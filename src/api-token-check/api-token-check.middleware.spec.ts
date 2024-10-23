e3b6b8fb1f5643abdb622564009a0fc1ed4148c1
dearM762 <misterfighter1990@gmail.com>
2024-10-23 18:39:42 +0500

    create auth route

--- src/api-token-check/api-token-check.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ApiTokenCheckMiddleware } from './api-token-check.middleware';
-
-describe('ApiTokenCheckMiddleware', () => {
-  it('should be defined', () => {
-    expect(new ApiTokenCheckMiddleware()).toBeDefined();
-  });
-});
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { TestModule } from './pipes/test.module';
 import { MidModule } from './mid/mid.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, TestModule, MidModule],
+  imports: [ProductModule, TestModule, MidModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
