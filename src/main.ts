c502938408bf7e47c5bc5af371d2b42bdc9ebfc6
dearM762 <misterfighter1990@gmail.com>
2024-10-25 01:07:01 +0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Reminder SuperApp')
+    .setDescription('The Reminder API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
   app.enableCors()
-  await app.listen(3000)
+  await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
